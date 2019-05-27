fed930a2d01deae49eef20520d9c52437c186d13
adam moran <ad_m123@hotmail.co.uk>
2019-05-27 22:59:32 +0100

    Initial rough implementation of Wiggo bot.

---
+++ WiggBot/Commands.cs
@@ -0,0 +1,148 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.EventArgs;
+using DSharpPlus.VoiceNext;
+using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WiggBot
+{
+    internal class Commands
+    {
+        private ConcurrentDictionary<uint, Process> mFfmpegs;
+
+        [Command("hi")]
+        public async Task Hi(CommandContext ctx)
+        {
+            await ctx.RespondAsync($"👋 Hi, {ctx.User.Mention}!");
+        }
+
+        [Command("join")]
+        public async Task Join(CommandContext ctx)
+        {
+            var vnext = ctx.Client.GetVoiceNextClient();
+
+            try
+            {
+                var vnc = vnext.GetConnection(ctx.Guild);
+                if (vnc != null)
+                    throw new InvalidOperationException("Already connected in this guild.");
+
+                var chn = ctx.Member?.VoiceState?.Channel;
+                if (chn == null)
+                    throw new InvalidOperationException("You need to be in a voice channel.");
+
+                vnc = await vnext.ConnectAsync(chn);
+
+                await Talk(ctx, "C:\\Temp\\aloha.wav");
+            }
+            catch (InvalidOperationException ex)
+            {
+                await ctx.RespondAsync(ex.Message);
+                throw;
+            }
+        }
+
+        [Command("leave")]
+        public async Task Leave(CommandContext ctx)
+        {
+            var vnext = ctx.Client.GetVoiceNextClient();
+
+            try
+            {
+                var vnc = vnext.GetConnection(ctx.Guild);
+                if (vnc == null)
+                    throw new InvalidOperationException("Not connected in this guild.");
+                this.mFfmpegs = null;
+                await Talk(ctx, "C:\\Temp\\goodbye.wav");
+                vnc.Disconnect();
+            }
+            catch (InvalidOperationException ex)
+            {
+                await ctx.RespondAsync(ex.Message);
+                throw;
+            }
+        }
+
+        [Command("matt")]
+        public async Task PlayMatt(CommandContext ctx)
+        {
+            await Talk(ctx, "C:\\Temp\\sweden.wav");
+        }
+
+        [Command("alone")]
+        public async Task PlayAlone(CommandContext ctx)
+        {
+            await Talk(ctx, "C:\\Temp\\alonenow.wav");
+        }
+
+        [Command("sorry")]
+        public async Task Play(CommandContext ctx)
+        {
+            await Talk(ctx, "C:\\Temp\\wormsorry.wav");
+        }
+
+        public async Task OnVoiceReceived(VoiceReceiveEventArgs ea)
+        {
+            if (!this.mFfmpegs.ContainsKey(ea.SSRC))
+            {
+                var psi = new ProcessStartInfo
+                {
+                    FileName = "ffmpeg",
+                    Arguments = $@"-ac 2 -f s16le -ar 48000 -i pipe:0 -ac 2 -ar 44100 {ea.SSRC}.wav",
+                    RedirectStandardInput = true
+                };
+
+                this.mFfmpegs.TryAdd(ea.SSRC, Process.Start(psi));
+            }
+
+            var buff = ea.Voice.ToArray();
+
+            var ffmpeg = this.mFfmpegs[ea.SSRC];
+            await ffmpeg.StandardInput.BaseStream.WriteAsync(buff, 0, buff.Length);
+            await ffmpeg.StandardInput.BaseStream.FlushAsync();
+        }
+
+        private static async Task Talk(CommandContext ctx, string file)
+        {
+            var vnext = ctx.Client.GetVoiceNextClient();
+
+            var vnc = vnext.GetConnection(ctx.Guild);
+            if (vnc == null)
+                throw new InvalidOperationException("Not connected in this guild.");
+
+            if (!File.Exists(file))
+                throw new FileNotFoundException("File was not found.");
+
+            //await ctx.RespondAsync("👌");
+            await vnc.SendSpeakingAsync(true); // send a speaking indicator
+
+            var psi = new ProcessStartInfo
+            {
+                FileName = "ffmpeg",
+                Arguments = $@"-i ""{file}"" -ac 2 -f s16le -ar 48000 pipe:1",
+                RedirectStandardOutput = true,
+                UseShellExecute = false
+            };
+            var ffmpeg = Process.Start(psi);
+            var ffout = ffmpeg.StandardOutput.BaseStream;
+
+            var buff = new byte[3840];
+            int br;
+            while ((br = ffout.Read(buff, 0, buff.Length)) > 0)
+            {
+                if (br < buff.Length) // not a full sample, mute the rest
+                    for (var i = br; i < buff.Length; i++)
+                        buff[i] = 0;
+
+                await vnc.SendAsync(buff, 20);
+            }
+
+            await vnc.SendSpeakingAsync(false); // we're not speaking anymore
+        }
+    }
+}
--- WiggBot/Program.cs
+++ WiggBot/Program.cs
@@ -1,12 +1,95 @@
-﻿using System;
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.VoiceNext;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace WiggBot
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static DiscordClient mDiscord;
+        private static CommandsNextModule mCommands;
+        private static VoiceNextClient mVoice;
+
+        private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            if (!TryReadConfiguration(out var token))
+            {
+                return;
+            }
+
+            var prog = new Program();
+            prog.MainAsync(token).GetAwaiter().GetResult();
+        }
+
+        private async Task MainAsync(string token)
+        {
+            mDiscord = new DiscordClient(new DiscordConfiguration
+            {
+                UseInternalLogHandler = true,
+                LogLevel = LogLevel.Debug,
+                Token = token,
+                TokenType = TokenType.Bot,
+            });
+
+            mCommands = mDiscord.UseCommandsNext(new CommandsNextConfiguration
+            {
+                EnableDms = true,
+                EnableMentionPrefix = true,
+                StringPrefix = ";;"
+            });
+
+            mVoice = mDiscord.UseVoiceNext(new VoiceNextConfiguration
+            {
+                EnableIncoming = true
+            });
+
+            mCommands.RegisterCommands<Commands>();
+
+            CreateMessageBindings();
+
+            await mDiscord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private static void CreateMessageBindings()
+        {
+            mDiscord.MessageCreated += async e =>
+            {
+                if (e.Message.Content.StartsWith("hippo", StringComparison.OrdinalIgnoreCase))
+                    await e.Message.RespondAsync("Matt Lawrence is shit at Fortnite");
+            };
+        }
+
+        private static bool TryReadConfiguration(out string token)
+        {
+            token = string.Empty;
+
+            try
+            {
+                using (var reader = new StreamReader("settings.json"))
+                {
+                    var content = reader.ReadToEnd();
+
+                    var configuration = JsonConvert.DeserializeObject<Configuration>(content);
+                    token = configuration.Token;
+
+                    return !String.IsNullOrEmpty(token);
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                return false;
+            }
+
+        }
+
+        private class Configuration
+        {
+            public string Token { get; set; }
         }
     }
 }
